1d411abe03fc7ddeda8f66e3c499bff26af21672
Chantel Wetzel <chantelwetzel@noaa.gov>
2021-12-10 11:04:24 -0800

    add: draft document using sa4ss

---
+++ doc/_create_doc.R
@@ -0,0 +1,22 @@
+# Always: Load packages
+library(sa4ss)
+
+# Always: Specify the directory for the document
+setwd("C:/Users/Chantel.Wetzel/Documents/GitHub/assessment_prioritization/doc")
+
+# Once: Define the title, species, and authors
+#       Required author names here: https://github.com/nwfsc-assess/sa4ss/blob/master/data-raw/authors.csv
+sa4ss::draft(authors = c("Chantel R. Wetzel", "Jim Hastie", "Kristin Marshall"),
+  			 species = "Copper Rockfish",
+  			 latin = "Sebastes caurinus",
+  			 coast = "Oregon US West",
+  			 type = c("sa"),
+  			 create_dir = FALSE,
+  			 edit = FALSE)
+
+# Render Call:
+if(file.exists("_main.Rmd")){
+	file.remove("_main.Rmd")
+}
+# Render the pdf
+bookdown::render_book("00a.Rmd", clean=FALSE, output_dir = getwd())
--- doc/rcode/preamble.R
+++
@@ -1,5 +0,0 @@
-ass.yr = 2021
-sum.yr = 2020
-# Summary start and end years
-start.yr = 2014
-end.yr = sum.yr - 2
