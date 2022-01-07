1f439df83378ed3d501a5b19eb332b8b09baf5c6
Chantel Wetzel <chantelwetzel@noaa.gov>
2022-01-07 11:54:24 -0800

    update call for all fxns

--- R/call.R
+++ R/call.R
@@ -8,7 +8,9 @@ species_file <-  "species_names.csv"
 # Set the years for the revenue summary, recreational importance
 years <- 2016:2020
 
+#============================================================================
 # Commercial Revenue:
+#============================================================================
 # CSV files pulled from PacFIN with
 # COUNCIL_CODE == "P" and !FLEET_CODE %in% c("TI", "R")
 summarize_revenue(file_name = "revenue_summarized_12052021.csv", 
@@ -16,7 +18,9 @@ summarize_revenue(file_name = "revenue_summarized_12052021.csv",
 	years = years, 
 	max_exp = 0.18)
 
+#============================================================================
 # Tribal revenue:
+#============================================================================
 # CSV files pulled from PacFIN with
 # COUNCIL_CODE == "P" and FLEET_CODE == "TI"
 summarize_revenue(file_name = "tribal_revenue_12062021.csv", 
@@ -24,7 +28,9 @@ summarize_revenue(file_name = "tribal_revenue_12062021.csv",
 	years = years, 
 	max_exp = 0.18)
 
+#============================================================================
 # Recreational Importance:
+#============================================================================
 # Landings data pulled from RecFIN for all states and species
 summarize_rec_importance(
 	file_name = "CTE002-2016---2020.csv", 
@@ -32,10 +38,12 @@ summarize_rec_importance(
 	years = years, 
 	max_exp = 0.18) 
 
+#============================================================================
 # Fishing mortality:
 # Large management quantity download from PacFIN (GMT016) 
 # provided by Rob Ames. Only single species information 
 # available for download as of December 2021
+#============================================================================
 
 # The nwfscSurvey package is loaded in order to pull GEMM data
 # which is the official mortality source
@@ -47,17 +55,84 @@ fish_mort <- summarize_fishing_mortality(
 	species_file = species_file, 
 	years = years)
 
+#============================================================================
 # Function that will clean up all folders in the "model_files"
 # folder to only keep the files needed for r4ss to read model results
+#============================================================================
 
 clean_model_files(loc = "model_files/")
 
+#============================================================================
 # Function to grab and summarize the newest assessment results which
-# will then update the abundcanc (stock status) and the assessment
+# will then update the abundcancd (stock status) and the assessment
 # frequency csvs from last cycle
+#============================================================================
+
 summarize_model_results(
 	abundance_file = "abundance_previous_cycle.csv", 
 	frequency_file = "assessment_frequency_last_cycle.csv", 
 	species_file = species_file, 
 	model_loc = "model_files", 
 	years = 2000:2020)
+
+#============================================================================
+# Do Ecosystem Call HERE
+#============================================================================
+
+#============================================================================
+# Do Future SPEX constraint mortality HERE
+#============================================================================
+
+manage_file <- "GMT008-harvest specifications-2024.csv"
+fishing_mort_file <- "fishing_mortality.csv"
+freq_file <- "assessment_frequency.csv"
+species_file <-  "species_names.csv"
+years <- c(2019, 2021)
+low_f_species <- TRUE
+
+#species_file <- "species_names_not_included.csv"
+#low_f_species = FALSE
+
+summarize_future_spex(
+	manage_file = manage_file, 
+	fishing_mort_file = fishing_mort_file, 
+	freq_file = freq_file, 
+	species_file = species_file, 
+	years = years, 
+	low_f_species = TRUE
+)
+
+#============================================================================
+# 
+#============================================================================
+
+frequency_file <- "frequency_partial.csv"
+ecosystem_file <- "ecosystem.csv"
+mortality_file <- "fishing_mortality.csv"
+future_mortality_file <- "fishing_mortality.csv"
+prioritization_year <- 2023
+
+summarize_frequency(
+	frequency_file = frequency_file, 
+	ecosystem_file = ecosystem_file, 
+	mortality_file = mortality_file, 
+	future_mortality_file = future_mortality_file, 
+	prioritization_year = prioritization_year, 
+	max_age = 20, 
+	age_exp = 0.38)
+
+#============================================================================
+# 
+#============================================================================
+
+commercial_file <- "commercial_revenue_with_gear_code.csv"
+rec_file <- "recreational_importance.csv"
+species_file <-  "species_names.csv"
+years <- 2016:2020
+
+summarize_const_demand(
+	commercial_name = commercial_name, 
+	rec_file = rec_file, 
+	species_file = species_file, 
+	years = years, 
+	max_exp = 0.18)
