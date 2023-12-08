9fc19b611199f35c26b425af6b39e802bb4f58f6
Chantel Wetzel <chantelwetzel@noaa.gov>
2023-12-08 15:42:07 -0800

    2024 revisions
    1. calculate both the commercial and recreational importance based on +1 if one area has greater importance > 0.10 than the cw
    2. add in future harvest spex limitations as the approach to calculate choke stocks.

--- R/summarize_const_demand.R
+++ R/summarize_const_demand.R
@@ -7,13 +7,10 @@
 #' 
 #' 
 #'
-#' @param commercial_file A csv file with commercial revenue and gear codes downloaded
-#' from PacFIN (via login account).
+#' @param revenue_data Filtered revenue from PacFIN that includes both commercial 
+#' and tribal revenue.
 #' @param rec_file A csv file with recreational importance factor calculation
 #' @param species_file A csv file with species names to summarize
-#' @param years Years to average the commercial revenue across.
-#' @param max_exp Numeric value that will be applied to create a spread across ranked 
-#' species calculated as: value ^ max_exp
 #'
 #' @author Chantel Wetzel
 #' @export
@@ -21,52 +18,53 @@
 #' 
 #' @examples
 #' 
-#' summarize_const_demand(
-#' 		commercial_file <- "commercial_revenue_with_gear_code.csv"
-#' 		rec_file <- "recreational_importance.csv"
-#' 		species_file <-  "species_names.csv"
-#' 		years <- 2016:2020
-#' )
 #'
-summarize_const_demand <- function(commercial_name, rec_file, species_file, years, max_exp = 0.18) {
+summarize_const_demand <- function(
+    revenue_data, 
+    rec_importance_data, 
+    fishing_mortality,
+    future_spex,
+    species = species) {
 
-	com_data <- read.csv(file.path("data", commercial_file)) 
-	rec_data <- read.csv(file.path("tables", rec_file)) 
-	species <-  read.csv(file.path("data", species_file))
-
-	com_data$gear <- "TWL"
-	com_data$gear[!com_data$PACFIN_GROUP_GEAR_CODE == "TWL"] <- "NTWL"
+	rec_data <- rec_importance_data 
+	revenue_data$gear <- "TWL"
+	revenue_data$gear[!revenue_data$PACFIN_GROUP_GEAR_CODE == "TWL"] <- "NTWL"
 
 	rec_data <- with(rec_data, rec_data[order(rec_data[,"Species"]), ])
 
-	data <- data.frame(Species = species[,1], 
-					   CW = NA, 
-					   C = NA,
-					   O = NA,
-					   W = NA, 
-					   TWL = NA, 
-					   NTWL = NA)
+	data <- data.frame(
+	  Species = species[,1], 
+	  Commercial_Importance_Modifier = 0,
+		CW = NA, 
+		C = NA,
+		O = NA,
+		W = NA, 
+		TWL = NA, 
+		NTWL = NA)
 
-	score_df <- rank_df <- data
-	rec_score_df <- rec_rank_df <- data[, -c(ncol(data)-1, ncol(data))]
+	com_importance_df <- score_rank_df <- data
+
+	rec_tmp <- rec_data[, c("Species", "Pseudo_Revenue_Coastwide", "Pseudo_Revenue_CA", "Pseudo_Revenue_OR", "Pseudo_Revenue_WA")]
+	colnames(rec_tmp)[2:5] <- c("CW", "C", "O", "W")
+	rec_score_df <- rec_importance_df <- rec_tmp #data[, -c(ncol(data)-1, ncol(data))]
 
 	denominator <- 1000
 	max_value <- 10
 
 	# Calculate commercial ranking and scores first
+	# This breaks things out by non-trawl and trawl but also includes both 
+	# commercial and tribal revenue combined by state
 	for(sp in 1:nrow(species)) {
 
 		key <- NULL
 		name_list <- species[sp, species[sp,] != -99]
 		for(a in 1:length(name_list)){
 			key = c(key,
-				grep(species[sp,a], com_data$NOMINAL_TO_ACTUAL_PACFIN_SPECIES_NAME, ignore.case = TRUE)
+				grep(species[sp,a], revenue_data$NOMINAL_TO_ACTUAL_PACFIN_SPECIES_NAME, ignore.case = TRUE)
 			)
 		}
 		
-		sub_data <- com_data[key,]
-		find <- which(sub_data$PACFIN_YEAR %in% years) 
-		sub_data <- sub_data[find, ]
+		sub_data <- revenue_data[key,]
 
 		tmp <- aggregate(AFI_EXVESSEL_REVENUE ~ AGENCY_CODE, sub_data, function(x) sum(x) / denominator )
 		data[sp, "CW"] <- sum(tmp$AFI_EXVESSEL_REVENUE) 
@@ -83,49 +81,130 @@ summarize_const_demand <- function(commercial_name, rec_file, species_file, year
 		data[sp, find] <- 0		
 	}
 
-	int_value <- data[, -1] ^ max_exp
-	for(a in 2:ncol(score_df)){
-		score_df[, a] <- int_value[, a-1] * max_value / max(int_value[, a-1])
-		tmp <- score_df[order(score_df[, a], decreasing = TRUE), c(1, a)]
+	#int_value <- data[, -1] #^ max_exp
+	for(a in 3:ncol(score_rank_df)){
+		score_rank_df[, a] <- round(max_value * log(data[, a] + 1)  / max(log(data[, a] + 1)), 3)
+		tmp <- score_rank_df[order(score_rank_df[, a], decreasing = TRUE), c(1, a)]
 		tmp$tmp_rank <- 1:nrow(tmp)
 		rank <- with(tmp, tmp[order(tmp[,"Species"]), ])
-		rank_df[, a] <- rank$tmp_rank
+		com_importance_df[, a] <- rank$tmp_rank
 
 		x <- 1
-		for(i in sort(unique(score_df[, a]), decreasing = TRUE)) {
-			ties <- which(score_df[, a] == i)
+		for(i in sort(unique(score_rank_df[, a]), decreasing = TRUE)) {
+			ties <- which(score_rank_df[, a] == i)
 			if(length(ties) > 0) {
-				rank_df[, a][ties] <- x
+				com_importance_df[, a][ties] <- x
 			}
 			x <- x + length(ties)
 		}
+		com_importance_df[, a] <- round(1 - com_importance_df[, a]/max(com_importance_df[, a]),3)
+		com_importance_df[, a] <- round(com_importance_df[, a]/max(com_importance_df[, a]),3)
 	}
-    
-    # Recreational importance
-    rec_tmp <- rec_data[, c("Pseudo_CW", "Pseudo_CA", "Pseudo_OR", "Pseudo_WA")]
-    rec_tmp[is.na(rec_tmp)] <- 0
-
-	int_value <- rec_tmp ^ max_exp
+	com_importance_df[, "Commercial_Importance_Modifier"] <- apply(com_importance_df[,c("C", "O", "W")] - com_importance_df[, "CW"] > 0.10, 1, sum) +
+	 ifelse(abs(com_importance_df[, "NTWL"] - com_importance_df[, "TWL"]) < 0.10, 1, 0)
+  
+	#===================================================  
+  # Recreational importance
+	#===================================================
+  
+	#int_value <- rec_tmp ^ max_exp
 	for(a in 2:ncol(rec_score_df)){
-		rec_score_df[, a] <- int_value[, a-1] * max_value / max(int_value[, a-1])
+		rec_score_df[, a] <-  round(max_value * log(rec_tmp[, a] + 1)  / max(log(rec_tmp[, a] + 1)), 3)
 		tmp <- rec_score_df[order(rec_score_df[, a], decreasing = TRUE), c(1, a)]
 		tmp$tmp_rank <- 1:nrow(tmp)
 		rank <- with(tmp, tmp[order(tmp[,"Species"]), ])
-		rec_rank_df[, a] <- rank$tmp_rank
+		rec_importance_df[, a] <- rank$tmp_rank
 
 		x <- 1
 		for(i in sort(unique(rec_score_df[, a]), decreasing = TRUE)) {
 			ties <- which(rec_score_df[, a] == i)
 			if(length(ties) > 0) {
-				rec_rank_df[, a][ties] <- x
+				rec_importance_df[, a][ties] <- x
 			}
 			x <- x + length(ties)
 		}
+		rec_importance_df[, a] <- round(1 - rec_importance_df[, a]/max(rec_importance_df[, a]), 3)
+		rec_importance_df[, a] <- round(rec_importance_df[, a]/max(rec_importance_df[, a]), 3)
 	}
+  rec_importance_df$Recreational_Importance_Modifier <- apply(rec_importance_df[,c("C", "O", "W")] - rec_importance_df[, "CW"] > 0.10, 1, sum) 
+  
+  #====================================================================================
+  # Choke stock - Pull in the fishing mortality tab and use that information or could
+  #====================================================================================
+  # use only the future spex modifier section
+  # Pull in rebuilding tab
+  # Sum the commercial and recreational adjustments, choke stock, and rebuilding
+  # Rank the scores
+  choke_df <- data.frame(
+    Species = species[,1],
+    Average_Removals = fishing_mortality$Average_Removals,
+    Future_ACL_Attainment = NA,
+    Choke_Stock_Score = 0
+  )
+  
+  for(sp in 1:nrow(species)) {
+    
+    ff <- NULL
+    name_list <- species[sp, species[sp,] != -99]
+    
+    for(a in 1:length(name_list)){
+      ff <- c(ff, grep(species[sp, a], future_spex$STOCK_OR_COMPLEX, ignore.case = TRUE))
+    }
+    if (length(ff) == 0){
+      for(a in 1:length(name_list)){
+        init_string <- tm::removeWords(species[sp, a], " rockfish")
+        ff <- c(ff, grep(init_string, future_spex$STOCK_OR_COMPLEX, ignore.case = TRUE))
+      }
+    }
+    
+    ff <- unique(ff)
+    
+    # Future Attainment
+    choke_df$Future_ACL_Attainment[sp] <- choke_df$Average_Removals[sp] / sum(future_spex[ff, "ACL"], na.rm = TRUE)
+    
+    # Calculate the adjustment based on future spex limitations
+    choke_df[sp, "Choke_Stock_Score"] <-
+      ifelse(choke_df$Future_ACL_Attainment[sp] >= 1.25, 5,
+             ifelse(choke_df$Future_ACL_Attainment[sp] < 1.25 & choke_df$Future_ACL_Attainment[sp] >= 1, 4,
+             ifelse(choke_df$Future_ACL_Attainment[sp] < 1.0 & choke_df$Future_ACL_Attainment[sp] >= 0.90, 3,
+                    ifelse(choke_df$Future_ACL_Attainment[sp] < 0.9 & choke_df$Future_ACL_Attainment[sp] >= 0.80, 2, 
+                           ifelse(choke_df$Future_ACL_Attainment[sp] < 0.8 & choke_df$Future_ACL_Attainment[sp] >= 0.70, 2, 0)))))
+  }
+  
+  
+  const_importance <- data.frame(
+    Species = species[,1],
+    Rank = NA,
+    Score = choke_df$Choke_Stock_Score + com_importance_df$Commercial_Importance_Modifier + rec_importance_df$Recreational_Importance_Modifier,
+    Choke_Stock_Score = choke_df$Choke_Stock_Score,
+    Commerical_Importance_Score = com_importance_df$Commercial_Importance_Modifier,
+    Recreational_Importance_Score = rec_importance_df$Recreational_Importance_Modifier,
+    Futue_ACL_Attainment = round(100 * choke_df$Future_ACL_Attainment, 0)
+  )
+  
+  const_importance <- const_importance[order(const_importance[,"Score"], decreasing = TRUE), ]
+  
+  zz <- 1
+  max_range <- ifelse(min(const_importance$Score) != 0, max(const_importance$Score), max(const_importance$Score) + 1)
+  for(i in max_range:1) {
+    if(min(const_importance$Score) == 0){
+      ties <- which(const_importance$Score == (i-1))
+    } else {
+      ties <- which(const_importance$Score == i)
+    }
+    if(length(ties) > 0) {
+      const_importance$Rank[ties] <- zz
+    }
+    zz <- zz + length(ties)
+  }
 
-	write.csv(score_df, file.path("tables", "const_demand_commercial_scores.csv"), row.names = FALSE)
-	write.csv(rec_score_df, file.path("tables", "const_demand_recreational_scores.csv"), row.names = FALSE)
-	write.csv(rank_df, file.path("tables", "const_demand_commercial_ranks.csv"), row.names = FALSE)
-	write.csv(rec_rank_df, file.path("tables", "const_demand_recreational_ranks.csv"), row.names = FALSE)
+  const_importance <- const_importance[order(const_importance[,"Species"], decreasing = FALSE), ]
+  write.csv(const_importance, file.path("data-processed", "8_constituent_demand.csv"), row.names = FALSE)
+  write.csv(rec_importance_df, file.path("data-processed", "_constituent_demand_rec_importance.csv"), row.names = FALSE)
+  write.csv(com_importance_df, file.path("data-processed", "_constituent_demand_com_importance.csv"), row.names = FALSE)
+	#write.csv(com_importance_df, file.path("tables", "const_demand_commercial_scores.csv"), row.names = FALSE)
+	#write.csv(rec_score_df, file.path("tables", "const_demand_recreational_scores.csv"), row.names = FALSE)
+	#write.csv(rank_df, file.path("tables", "const_demand_commercial_ranks.csv"), row.names = FALSE)
+	#write.csv(rec_importance_df, file.path("tables", "const_demand_recreational_ranks.csv"), row.names = FALSE)
 
 }
